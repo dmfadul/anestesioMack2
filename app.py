931d55cd3a108063cd4a3b949b0d820c4f91fe37
doctormatterhorn <doctormatterhorn@gmail.com>
2023-10-14 17:26:19 -0300

    added signup page

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def hello_world():
+    if request.method == "POST":
+        name = request.form.get("name")
+        password = request.form.get("password")
+        print(name, password)
     return render_template("home.html")
+
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    return render_template("signup.html")
